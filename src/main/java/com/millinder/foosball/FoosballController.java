388250d0afa19a45ed412fd3f99dc14af018d247
Benjamin <benjaminjacobberg@gmail.com>
2020-10-08 11:12:14 -0400

    Getting things running again

--- src/main/java/com/millinder/foosball/FoosballController.java
+++ src/main/java/com/millinder/foosball/FoosballController.java
@@ -1,21 +1,18 @@
 package com.millinder.foosball;
 
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FoosballController {
-    @RequestMapping(path="/foos", method= RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    @PostMapping(path = "/foos", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     public String newMatchRequest(SlackCommandData data) {
-         return data.getUser_name() + " is looking for players";
+        return data.getUser_name() + " is looking for players";
     }
 
-    @RequestMapping(path="/play", method= RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    @PostMapping(path = "/play", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     public String joinMatch(SlackCommandData data) {
-         return data.getUser_name() + " wants to play";
+        return data.getUser_name() + " wants to play";
     }
-
-    
 }
--- src/test/java/com/millinder/foosball/FoosballApplicationTests.java
+++ src/test/java/com/millinder/foosball/FoosballApplicationTests.java
@@ -1,16 +1,13 @@
 package com.millinder.foosball;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class FoosballApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
